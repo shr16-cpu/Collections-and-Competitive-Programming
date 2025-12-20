1ee667753f354acf2d5e14c5ed9c987510cd7117
shraddha <gendle.shraddha16@gmail.com>
2025-12-20 11:58:28 +0530

    changes

---
+++ src/main/java/org/example/CountDigits.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        int n = 1234;
+        int count = 0;
+        while(n!=0){
+            count++;
+            n=n/10;
+
+        }
+        System.out.println(count);
+    }
+}
